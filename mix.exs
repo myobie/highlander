4153129132afb6dcd0b205b5b2d8fd04de64809e
Nathan Herald <me@nathanherald.com>
2017-01-01 17:06:26 +0100

    Compile test/support

--- mix.exs
+++ mix.exs
@@ -7,8 +7,12 @@ defmodule Highlander.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     elixirc_paths: elixirc_paths(Mix.env),
      deps: deps()]
   end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_),     do: ["lib"]
 
   def application do
     [
